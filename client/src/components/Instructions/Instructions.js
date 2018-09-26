a39e3a9698eeaa1a9e3918ef71bf59b99ead5982
Christian Escobar <crs.escobar@gmail.com>
2018-09-26 16:13:24 -0700

    card title css for instructions

--- client/src/components/Instructions/Instructions.js
+++ client/src/components/Instructions/Instructions.js
@@ -9,7 +9,7 @@ const Instructions = (props) => {
           <div className='col-md-6'>
             <div className='card step-card'>
               <div className="card-body">
-                <h5 className="card-title">1</h5>
+                <h5 className="card-title card-instruction">1</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Personal Data</h6>
                 <p className="card-text">Tell us who you are!  Start out by providing some basic details to help us identify you.</p>
               </div>
@@ -18,7 +18,7 @@ const Instructions = (props) => {
           <div className='col-md-6'>
             <div className='card step-card'>
               <div className="card-body">
-                  <h5 className="card-title">2</h5>
+                  <h5 className="card-title card-instruction">2</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Address</h6>
                   <p className="card-text">Show me the property!  Now, tell use the location of the property available for rent.</p>
               </div>      
@@ -29,7 +29,7 @@ const Instructions = (props) => {
           <div className='col-md-6'>
             <div className='card step-card mt-4'>
               <div className="card-body">
-                  <h5 className="card-title">3</h5>
+                  <h5 className="card-title card-instruction">3</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Expected Rent</h6>
                   <p className="card-text">Now, tell us how much!  The next step is to provide us with the expected rent amount.</p>
               </div>
@@ -38,7 +38,7 @@ const Instructions = (props) => {
           <div className='col-md-6'>
             <div className='card step-card mt-4'>
               <div className="card-body">
-                  <h5 className="card-title">4</h5>
+                  <h5 className="card-title card-instruction">4</h5>
                   <h6 className="card-subtitle mb-2 text-muted">Confirmation Email</h6>
                   <p className="card-text">That's it!  Once you have completed the steps above, you will receive an email confirmation.</p>
               </div>
