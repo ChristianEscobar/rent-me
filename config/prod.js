71bde93bcf54c76e8b3bd43a80504a8bfc9e9ac2
Christian Escobar <crs.escobar@gmail.com>
2018-09-26 13:27:10 -0700

    update .gitignore for config/dev.js

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googlePlacesApiKey: process.env.GOOGLE_PLACES_API_KEY,
+  zillowApiKey: process.env.ZILLOW_API_KEY,
+  emailHost: process.env.EMAIL_HOST,
+  emailPort: proces.env.EMAIL_PORT,
+  emailUser: process.env.EMAIL_USER,
+  emailPassword: process.env.EMAIL_PASSWORD,
+  emailSenderAddress: process.env.SENDER_ADDRESS
+};
