dae14fb994d459ace95029b0a638ff3a78c89bad
Christian Escobar <crs.escobar@gmail.com>
2018-09-26 14:37:45 -0700

    securing API keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,17 @@
+/*
 if (process.env.NODE_env === "production") {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
 }
+*/
+
+module.exports = {
+  googlePlacesApiKey: process.env.GOOGLE_PLACES_API_KEY,
+  zillowApiKey: process.env.ZILLOW_API_KEY,
+  emailHost: process.env.EMAIL_HOST,
+  emailPort: process.env.EMAIL_PORT,
+  emailUser: process.env.EMAIL_USER,
+  emailPassword: process.env.EMAIL_PASSWORD,
+  emailSenderAddress: process.env.SENDER_ADDRESS
+};
