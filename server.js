b3fa8626aeba403375784893ab789c8a7d5a4878
Christian Escobar <crs.escobar@gmail.com>
2018-09-26 14:42:15 -0700

    deployment fixes

--- server.js
+++ server.js
@@ -141,6 +141,12 @@ app.post('/api/send-email', (req, res) => {
   }
 });
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 //const port = process.env.PORT || 5000;
 
 module.exports = app;
